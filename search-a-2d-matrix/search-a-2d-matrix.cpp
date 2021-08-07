1caf163208e87f3e574e1e9ad46de41b7bcde909
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2021-08-07 16:47:00 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (49.90%) 👀

---
+++ search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(!matrix.size()) return false;
+        
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int low=0;
+        int high=(n*m)-1;
+        while(low<=high)
+        {
+            //make imaginary index from 0 till m*n to make the matrix as an array and do binary search on it
+            int mid=(low+high)/2;
+            if(matrix[mid/m][mid%m]==target)//calculate index
+                return true;
+            if(matrix[mid/m][mid%m]>target)
+                high=mid-1;
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return false;
+        
+    }
+};
