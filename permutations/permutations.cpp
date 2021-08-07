97c0f04f0a2721eca1c2863ad2da6e82256c1103
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2021-08-07 22:50:02 +0530

    Time: 4 ms (73.61%), Space: 7.7 MB (78.03%) 👀

---
+++ permutations/permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(int index,vector<int>&nums,vector<vector<int>>&ans)
+    {
+        if(index==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=index;i<nums.size();i++)
+        {
+            swap(nums[index],nums[i]);
+            solve(index+1,nums,ans);
+            swap(nums[index],nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) 
+    { 
+        vector<vector<int>>ans;
+        solve(0,nums,ans);
+        return ans;
+        
+    }
+    
+};
