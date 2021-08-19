3829c5aa144fe3d6f6cb7b1a99725021b36d418b
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2021-08-20 00:01:58 +0530

    Time: 16 ms (80.59%), Space: 19.7 MB (8.68%) 👀

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
+//array[]/7 7 5 5| 7 6|5 5 7 7| 7 7 7
+//candid//7 7 7 7  7 7 5 5 5 5  7 7 7 
+//count// 1 2 1 0  1 0 1 2 1 0  1 2 3   
         int candi=0;
         int count=0;
         for(int i:nums)
