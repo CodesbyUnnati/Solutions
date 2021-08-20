c6e54654539da27f9dc4ffeaaf2c21de9559d2cd
Unnati Mishra <54355445+CodesbyUnnati@users.noreply.github.com>
2021-08-20 23:59:51 +0530

    Time: 4 ms (59.88%), Space: 7.2 MB (31.37%) 👀

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
+    //Time complexity= O(2N^2)
+    //Space= O(1)
+        void rotate(vector<vector<int>>& matrix) {
+            int n=matrix.size();
+            for(int i=0;i<n;i++)
             {
-                swap(matrix[i][j],matrix[j][i]);
+                //TRANSPOSE AND REVERSE
+                for(int j=0;j<i;j++)
+                {
+                    swap(matrix[i][j],matrix[j][i]);
+                }
             }
+             for(int i=0;i<n;i++)
+             {
+
+            reverse(matrix[i].begin(),matrix[i].end());
+             }
         }
-         for(int i=0;i<n;i++)
-         {
-         
-        reverse(matrix[i].begin(),matrix[i].end());
-         }
-    }
 };
